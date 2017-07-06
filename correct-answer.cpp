2bb7965b4cc3d1eab0d9471808eea7f72f099466
James Le <le_j6@denison.edu>
2017-07-05 21:40:32 -0700

    Correct Answer

---
+++ correct-answer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int usernumber;
+  int correctanswer;
+  cout << "What is 2 + 2?";
+  cin << usernumber;
+  if (usernumber == correctanswer) {
+    cout << "Correct Answer!";
+  }
+}
